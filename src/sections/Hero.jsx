f167565b0b2d4369e4aceee85cadb5f09339ba28
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-07 22:29:31 +0530

    Added 3d model in hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,9 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import HackerRoom from "../components/HackerRoom"
+import CanvasLoader from "../components/CanvasLoader"
+import { Suspense } from "react"
+
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
@@ -6,6 +12,18 @@ const Hero = () => {
                 Hi, I am Arpit <span className="waving-hand">👋</span>
             </p>
             <p className="hero_tag text-gray_gradient">Aspiring Software Engineer</p>
+        </div>
+
+        <div className="w-full h-full absolute inset-0">
+          <Canvas className="w-full h-full">
+            <Suspense fallback={<CanvasLoader/>}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+
+            <HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 280, 0]}/>
+            <ambientLight intensity={1}/>
+            <directionalLight intensity={0.5} position={[10, 10, 10]}/>
+            </Suspense>
+          </Canvas>
         </div>
     </section>
   )
