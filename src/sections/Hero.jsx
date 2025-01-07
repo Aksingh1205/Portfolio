11f25c180ac4ed45e0776bb85a1173779ea717ca
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-07 21:16:43 +0530

    Updated Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
             <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
                 Hi, I am Arpit <span className="waving-hand">👋</span>
             </p>
+            <p className="hero_tag text-gray_gradient">Aspiring Software Engineer</p>
         </div>
     </section>
   )
