df9ad5effff88fbddeb485a82cd218d1fd2f65c5
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-14 19:52:12 +0530

    Updated About Section
    Added Globe threejs object
    added a div of timezone

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,5 @@
+import Globe from "react-globe.gl"
+
 const About = () => {
   return (
     <section className="c-space my-20" id="about">
@@ -22,6 +24,24 @@ const About = () => {
                     applications
                 </p>
                 </div>
+                </div>
+            </div>
+
+            <div className="col-span-1 xl:row-span-4">
+                <div className="grid-container">
+                    <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+                    <Globe
+                        height={326}
+                        width={326}
+                        backgroundColor="rgba(0, 0, 0, 0)"
+                        backgroundImageOpacity={0.5}
+                        showAtmosphere
+                        showGraticules
+                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                        labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                    />
+                    </div>
                 </div>
             </div>
         </div>
