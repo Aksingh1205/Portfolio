fb0539b340597e2a65f7802a79c0e210afff1488
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-15 22:59:50 +0530

    Added DemoComputer model

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,9 @@
+import { Canvas } from "@react-three/fiber";
 import { myProjects } from "../constants"
+import { Center, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+import DemoComputer from '../components/DemoComputer.jsx';
 
 const Projects = () => {
 
@@ -21,7 +26,7 @@ const Projects = () => {
 
                 <p className="animatedText">{currentProject.desc}</p>
                 <p className="animatedText">{currentProject.subdesc}</p>
-              </div>
+                </div>
 
               <div className="flex items-center justify-between flex-wrap gap-5">
               <div className="flex items-center gap-3">
@@ -51,8 +56,22 @@ const Projects = () => {
                   </button>
                 </div>
               </div>
+
+            <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+              <Canvas>
+                <ambientLight intensity={Math.PI} />
+                <directionalLight position={[10, 10, 5]} />
+                <Center>
+                  <Suspense fallback={<CanvasLoader />}>
+                    <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                      <DemoComputer texture={currentProject.texture} />
+                    </group>
+                  </Suspense>
+                </Center>
+                <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+              </Canvas>
             </div>
-        </div>
+          </div>
     </section>
   )
 }
