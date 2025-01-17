16910ca1fe3783b34f021da5c3d3df10ae82317f
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-17 20:32:45 +0530

    Updated Form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -30,8 +30,19 @@ const Contact = () => {
             <p className="text-lg text-white-600 mt-3">Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
             life, I’m here to help.</p>
 
-            <form>
-
+            <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col space-y-7 mt-12">
+                <label className="space-y-3">
+                    <span className="field-label">Full Name</span>
+                    <input type="text" name="name" value={form.name} onChange={handleChange} className="field-input" required placeholder="John Doe"/>
+                </label>
+                <label className="space-y-3">
+                    <span className="field-label">Email</span>
+                    <input type="email" name="email" value={form.email} onChange={handleChange} className="field-input" required placeholder="johndoe@gmail.com"/>
+                </label>
+                <label className="space-y-3">
+                    <span className="field-label">Your message</span>
+                    <textarea type="text" name="message" value={form.message} onChange={handleChange} className="field-input" required rows={5}   placeholder="Hi, I'm interested in..."/>
+                </label>
             </form>
         </div>
         </div>
