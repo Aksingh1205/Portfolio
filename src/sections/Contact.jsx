b646d49ef5853ae8d6c38e2e99af32bf827bf2cc
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-01-20 19:45:03 +0530

    Added Experience Section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
     //service_i43uhnn
 
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="contact">
         <div className="relative min-h-screen flex items-center justify-center flex-col">
         <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
 
@@ -56,7 +56,7 @@ const Contact = () => {
             <p className="text-lg text-white-600 mt-3">Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
             life, I’m here to help.</p>
 
-            <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col space-y-7 mt-12">
+            <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
                 <label className="space-y-3">
                     <span className="field-label">Full Name</span>
                     <input type="text" name="name" value={form.name} onChange={handleChange} className="field-input" required placeholder="John Doe"/>
